4aa4fc7b166da6ae02614346c2c909644082d852
hamoth <alexis.japas@protonmail.com>
2022-07-08 12:50:44 +0200

    Git genesis

--- src/napari_vesicles_segmentation/__init__.py
+++ src/napari_vesicles_segmentation/__init__.py
@@ -3,9 +3,8 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from ._widget import ExampleQWidget, example_magic_widget
+from ._widget import Segmentation2D
 
 __all__ = (
-    "ExampleQWidget",
-    "example_magic_widget",
+    "Segmentation2D"
 )
--- src/napari_vesicles_segmentation/_widget.py
+++ src/napari_vesicles_segmentation/_widget.py
@@ -1,46 +1,134 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
-"""
-from typing import TYPE_CHECKING
-
-from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
-
-if TYPE_CHECKING:
-    import napari
+import napari
+from napari.layers import Image
+from napari.qt.threading import thread_worker
+from magicgui import magicgui
+from skimage import filters, transform
+import skimage.morphology as morph
+from skimage import measure
+import scipy.ndimage as ndi
+import numpy as np
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
+def detect_cell(im: np.ndarray, membrane_erosion: int, closing_size: int, n_sigma: int, downsizing_ratio: int) -> np.ndarray:
+    """
+    This function detects the cell in the image using the Otsu thresholding method. The external membrane is removed by closing, filling holes and eroding the detected cell. For faster computation, these operations can be performed in a downsampled image.
+    :param im: The image to detect the cell in.
+    :param membrane_erosion: The size of the disk radius used for eroding the cell. This is used to remove the external membrane. This parameter scales when downsizing the image, for more information see 'downsizing_ratio' parameter.
+    :param closing_size: The size of the disk radius used for closing the cell. This is used to fill holes in the cell. This parameter scales when downsizing the image, for more information see 'downsizing_ratio' parameter.
+    :param n_sigma: If set to zero, no standardization is performed. Otherwise, the standard deviation of the image is set to n_sigma * the standard deviation of the image, the image is standardized and its values are clipped to the range [-1, 1] in order to remove outliers. The higher the value of n_sigma, the less outliers are removed. This operation can lead to a better detection of the cell.
+    :param downsizing_ratio: The downsampling ratio used for the downsampled image. This is used to speed up the computation. Downsampling the image have impact in reducing the resolution of erosion and closing e.g. for a downsize ratio of 2, setting the erosion size to 3 will result in an erosion size of 6.
+    :return: A binary image representing the cell detected mask.
+    """
+    # Downsize the image for faster computation
+    downsized_im = transform.resize(im, (im.shape[1] // downsizing_ratio, im.shape[0] // downsizing_ratio), anti_aliasing=False)
+
+    # Standardize and clip the image values to remove extreme ones
+    if n_sigma > 0:
+        downsized_im = (downsized_im - np.mean(downsized_im)) / (np.std(downsized_im) * n_sigma)
+        downsized_im = downsized_im.clip(min=-1, max=1)
+    # Normalize the downsized_im between 0 and 1
+    downsized_im = (downsized_im - downsized_im.min()) / (downsized_im.max() - downsized_im.min())
+
+    # Detect the cell using Otsu thresholding and apply morphological operations in order to remove the external membrane
+    cell = downsized_im > filters.threshold_otsu(downsized_im)
+    if closing_size > 0:
+        cell = morph.binary_closing(cell, morph.disk(closing_size))
+    cell = ndi.binary_fill_holes(cell)
+    if membrane_erosion > 0:
+        cell = morph.binary_erosion(cell, morph.disk(membrane_erosion))
+
+    # Upsize the image to original size and return it as a binary image
+    return transform.resize(cell.astype(np.uint8), (im.shape[1], im.shape[0]), anti_aliasing=False) > 0
+
+
+class Segmentation2D():
+    # Constructor
+    def __init__(self, viewer: napari.Viewer):
         super().__init__()
-        self.viewer = napari_viewer
+        self.viewer = viewer
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+    # Segmentation method
+    def segment(self, image: Image, min_size: int= 0, membrane_erosion: int=0, closing_size: int=0, n_sigma: int=3, downsizing_ratio: int=1, display_cell_detection: bool=False):
+        """
+        This function segments vesicles in the image by detecting the cell and then detecting the vesicles.
+        The cell is detected using the Otsu thresholding method. The external membrane is removed by closing, filling holes and eroding the detected cell. For faster computation, these operations can be performed in a downsampled image.
+        The vesicles are detected by performing an Otsu thresholding on the detected cell.
+        This function adds the detected vesicles or the detected cell (set in the display_cell_detection parameter) to the viewer.
+        :param image: The image to segment vesicles in. The image can be a 2D or 3D temporal stack of images.
+        :param min_size: The minimum size of the vesicles to detect. Smaller detected vesicles are removed.
+        :param membrane_erosion: The size of the disk radius used for eroding the cell. This is used to remove the external membrane. This parameter scales when downsizing the image, for more information see 'downsizing_ratio' parameter.
+        :param closing_size: The size of the disk radius used for closing the cell. This is used to fill holes in the cell. This parameter scales when downsizing the image, for more information see 'downsizing_ratio' parameter.
+        :param n_sigma: If set to zero, no standardization is performed. Otherwise, the standard deviation of the image is set to n_sigma * the standard deviation of the image, the image is standardized and its values are clipped to the range [-1, 1] in order to remove outliers. The higher the value of n_sigma, the less outliers are removed. This operation can lead to a better detection of the cell.
+        :param downsizing_ratio: The downsampling ratio used for the downsampled image. This is used to speed up the computation. Downsampling the image have impact in reducing the resolution of erosion and closing e.g. for a downsize ratio of 2, setting the erosion size to 3 will result in an erosion size of 6.
+        :param display_cell_detection: If set to True, the cell detection is displayed in the viewer instead of the vesicle detection.
+        """
+        def _add_labels(labels: tuple):
+            """
+            This function adds the labels to the viewer. It is used as a callback function for the thread_worker. The labels in the first element of the tuple are added to the viewer as a label layer with the given name in the second element of the tuple.
+            :param labels: A tuple containing the labels and the name of the layer.
+            """
+            self.viewer.add_labels(labels[0], name=labels[1])
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        @thread_worker(connect={"returned": _add_labels})
+        def _segment():
+            """
+            This function performs the computation of the segmentation of the vesicles of the given image in a separate thread to avoid blocking the GUI.
+            :return: A tuple containing the labels and the name of the label layer.
+            """
+            ############################################################################################################
+            #### Preprocessing
+            ##################
+            # If the image is single, convert it to a stack with a single slice
+            if len(image.data.shape) == 2:
+                stack = np.extend(image.data, axis=0)
+            else:
+                stack = image.data
+            # Normalize the image data between 0 and 1
+            stack = (stack - stack.min()) / (stack.max() - stack.min())
+            
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+            ############################################################################################################
+            #### Segmentation
+            #################
+            # Detect the cell and erode the membrane if needed
+            max_intensity_stack = np.max(stack, axis=0)
+            max_intensity_cell = detect_cell(max_intensity_stack, membrane_erosion, closing_size, downsizing_ratio=1, n_sigma=n_sigma)
+            # Segmentation of the vesicles
+            vesicles_threshold = filters.threshold_otsu(max_intensity_stack[max_intensity_cell])
+            vesicles = stack > vesicles_threshold
+
+            ############################################################################################################
+            #### Postprocessing
+            ###################
+            # Cell membrane detection and erosion
+            for index in range(len(vesicles)):
+                cell = detect_cell(stack[index], membrane_erosion, closing_size, downsizing_ratio=downsizing_ratio, n_sigma=n_sigma)
+                if display_cell_detection:
+                    vesicles[index] = cell
+                else:
+                    vesicles[index] *= cell
+            # Remove small vesicles and false positives
+            if min_size > 0:
+                for index in range(len(vesicles)):
+                    vesicles[index] = morph.remove_small_objects(vesicles[index], min_size=min_size)
+
+            ############################################################################################################
+            #### Return the result
+            ######################
+            labels = measure.label(vesicles.astype(np.uint8).squeeze())
+            labels_name = f"{image.name}_{'cell' if display_cell_detection else 'vesicle'}"
+            return labels, labels_name
+        _segment()
 
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+if __name__ == "__main__":
+    # Creates a viewer
+    viewer = napari.Viewer()
 
+    # Adds segmentation widget to the viewer
+    segmentator = Segmentation2D(viewer)
+    segmentationWidget = magicgui(segmentator.segment, call_button="Segment", min_size={'label': 'minimum vesicles size'}, n_sigma={'label': 'clip[std]'}, downsizing_ratio={'min': 1})
+    viewer.window.add_dock_widget(segmentationWidget, name="Vesicles segmentation", add_vertical_stretch=True)
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+    # Run napari
+    napari.run()
