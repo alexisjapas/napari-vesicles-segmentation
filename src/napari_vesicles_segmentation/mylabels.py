8714ae36073697afbb70e92d56b7fbbfd41872c7
andreabassi78 <andreabassi@polimi.it>
2022-07-11 17:06:08 +0200

    my label saved wf data

--- src/napari_vesicles_segmentation/mylabels.py
+++ src/napari_vesicles_segmentation/mylabels.py
@@ -16,7 +16,8 @@ import os
 
 @magicgui(call_button='Extract',)
 def extract_rois(labels_layer:Labels,
-                 folder=pathlib.Path(os.getcwd()),
+                 source_folder=pathlib.Path(os.getcwd()),
+                 destination_folder=pathlib.Path(os.getcwd()),
                  sizez=10,
                  sizexy =30):
     from skimage.measure import regionprops_table
@@ -31,20 +32,34 @@ def extract_rois(labels_layer:Labels,
     ys = centroids['centroid-1']
     xs = centroids['centroid-2']
     areas = centroids['area']
-    name = labels_layer.name.replace('_vesicles','')
+    sim_name = labels_layer.name.replace('_vesicles','')
+    wf_name = sim_name.replace('_sim','_wf')
     
-    stack = viewer.layers[name].data
+    
+    stack = viewer.layers[sim_name].data
     index = 0
+    
+    wf_path = os.path.join(source_folder, f'{wf_name}.tif') 
+    wf_stack = tiff.imread(wf_path)
+    
+    
     for z,y,x,area in zip(zs,ys,xs,areas):
-        roi = stack[int(z-sizez//2):int(z+sizez//2),
+        sim_roi = stack[int(z-sizez//2):int(z+sizez//2),
                     int(y-sizexy//2):int(y+sizexy//2),
                     int(x-sizexy//2):int(x+sizexy//2),
                     ]
-        if not os.path.isdir(folder):
-            os.makedirs(folder)
-        path = os.path.join(folder, f'{name}_{index}.tif') 
+        wf_roi = wf_stack[int(z-sizez//2):int(z+sizez//2),
+                    int(y-sizexy//2):int(y+sizexy//2),
+                    int(x-sizexy//2):int(x+sizexy//2),
+                    ]
+        if not os.path.isdir(destination_folder):
+            os.makedirs(destination_folder)
+        destination_sim_path = os.path.join(destination_folder, f'{sim_name}_{index}.tif') 
+        destination_wf_path = os.path.join(destination_folder, f'{wf_name}_{index}.tif') 
+        
         index +=1
-        tiff.imwrite(path, roi)
+        tiff.imwrite(destination_sim_path, sim_roi)
+        tiff.imwrite(destination_wf_path, wf_roi)
     print(f'Saved {index} ROIs')
     
 
